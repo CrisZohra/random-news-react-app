cf9a2ecb4f98c7b837916779bf9b1563b4234452
Zohra <zohra.boukhatem1@gmail.com>
2023-11-17 18:07:31 +0100

    added functionality for filtering posts

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,12 @@
 import axios from "axios";
-import Navbar from "../components/Navbar";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function HomePage() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [location, setLocation] = useState("");
   const [date, setDate] = useState("");
-  const [category, setCategory] = useState("");
+  const [category, setCategory] = useState("other");
   const [imageURL, setImageURL] = useState("");
   const [URL, setURL] = useState("");
 
@@ -23,34 +22,39 @@ function HomePage() {
       imageURL,
       URL,
     };
+    console.log(requestBody);
 
     axios
       .post("https://random-news-react-app.adaptable.app/posts", requestBody)
+      .then(() => {
+        console.log("form submitted");
+
+        setTitle("");
+        setDescription("");
+        setLocation("");
+        setDate("");
+        setCategory("");
+        setImageURL("");
+        setURL("");
+      })
       .catch((error) => {
         console.log("Error creating new post...");
         console.log(error);
       });
-
-    setTitle("");
-    setDescription("");
-    setLocation("");
-    setDate("");
-    setCategory("");
-    setImageURL("");
-    setURL("");
   };
 
   return (
     <>
-    <h1>Random News; the Portal</h1>
+      <h1>Random News; the Portal</h1>
 
-    <h2>Submit your own news snippet!</h2>
+      <h2>Submit your own news snippet!</h2>
       <form onSubmit={handleSubmit}>
         <label className="form">
           Title:
           <input
             type="text"
             name="title"
+            placeholder="Enter title"
             required={true}
             value={title}
             onChange={(e) => {
@@ -58,17 +62,13 @@ function HomePage() {
             }}
           />
         </label>
-<<<<<<< HEAD
-        <br/>
-        <label className="form">
-=======
         <br />
-        <label>
->>>>>>> 64a4cdf8e341cccb31c3ea1c4b7dc0de590d553a
+        <label className="form">
           Description:
           <textarea
             type="text"
             name="description"
+            placeholder="Write a description"
             required={true}
             value={description}
             onChange={(e) => {
@@ -76,83 +76,60 @@ function HomePage() {
             }}
           />
         </label>
-<<<<<<< HEAD
-        <br/>
-        <label className="form">
-=======
         <br />
-        <label>
->>>>>>> 64a4cdf8e341cccb31c3ea1c4b7dc0de590d553a
+        <label className="form">
           Location:
           <input
             type="text"
             name="location"
+            placeholder="Specify the location"
             value={location}
             onChange={(e) => {
               setLocation(e.target.value);
             }}
           />
         </label>
-<<<<<<< HEAD
-        <br/>
-        <label className="form">
-=======
         <br />
-        <label>
->>>>>>> 64a4cdf8e341cccb31c3ea1c4b7dc0de590d553a
+        <label className="form">
           Date:
           <input
             type="date"
             name="date"
+            placeholder="Set the date"
             value={date}
             onChange={(e) => {
               setDate(e.target.value);
             }}
           />
         </label>
-<<<<<<< HEAD
-        <br/>
-        <label className="form">
-=======
         <br />
-        <label>
->>>>>>> 64a4cdf8e341cccb31c3ea1c4b7dc0de590d553a
+        <label className="form">
           Image:
           <input
             type="text"
             name="imageURL"
-            placeholder="paste image URL"
+            placeholder="Paste image URL"
             value={imageURL}
             onChange={(e) => {
               setImageURL(e.target.value);
             }}
           />
         </label>
-<<<<<<< HEAD
-        <br/>
-        <label className="form">
-=======
         <br />
-        <label>
->>>>>>> 64a4cdf8e341cccb31c3ea1c4b7dc0de590d553a
+        <label className="form">
           URL:
           <input
             type="text"
             name="URL"
-            placeholder=""
+            placeholder="Add a URL"
             value={URL}
             onChange={(e) => {
               setURL(e.target.value);
             }}
           />
         </label>
-<<<<<<< HEAD
-        <br/>
-        <label className="form">
-=======
         <br />
-        <label>
->>>>>>> 64a4cdf8e341cccb31c3ea1c4b7dc0de590d553a
+        <label className="form">
           Choose Category:
           <select
             name="category"
@@ -161,15 +138,15 @@ function HomePage() {
               setCategory(e.target.value);
             }}
           >
-            <option>Weather</option>
-            <option>Selling</option>
-            <option>Entertainment</option>
-            <option>Events</option>
-            <option>Traffic</option>
-            <option>Social</option>
-            <option>Jobs</option>
-            <option>Education</option>
-            <option>Other</option>
+            <option value="weather">Weather</option>
+            <option value="selling">Selling</option>
+            <option value="entertainment">Entertainment</option>
+            <option value="events">Events</option>
+            <option value="traffic">Traffic</option>
+            <option value="social">Social</option>
+            <option value="jobs">Jobs</option>
+            <option value="education">Education</option>
+            <option value="other">Other</option>
           </select>
         </label>
         <br />
--- src/pages/WeatherPostsPage.jsx
+++ src/pages/WeatherPostsPage.jsx
@@ -1,6 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function WeatherPostsPage () {
+    const [weatherPosts, setWeatherPosts] = useState([])
+    
+    useEffect(()=> {
+        axios.get("https://random-news-react-app.adaptable.app/posts/")
+        .then((response) => {
+            const array = response.data
+            console.log(array)
+        const weatherArray =  array.filter((elm) => {
+        return elm.category.toLowerCase() === "weather" 
+        })
+        setWeatherPosts(weatherArray)
+    })
+    .catch(error => {
+        console.log("Cannot filter posts", error)
+    })
+}, [])
+
+    console.log(weatherPosts)
+    console.log(weatherPosts.length)
+
     return(
+        <>
         <h1>Weather Posts</h1>
+    </>
     )
 }
 
