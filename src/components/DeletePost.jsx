e49f46a76cff3fc074cdae7437a90123b6f0b724
Cristina RP <cristinarp95@gmail.com>
2023-11-20 12:57:13 +0100

    Add confirm delete window

--- src/components/DeletePost.jsx
+++ src/components/DeletePost.jsx
@@ -20,7 +20,20 @@ function DeletePost({ postId, onDelete }) {
       });
   }
 
-  return <button onClick={deletePost}>Delete</button>;
+  const handlePostDelete = () => {
+    const shouldDelete = window.confirm(
+      "Are you sure you want to delete this post?"
+    );
+    if (shouldDelete) {
+      deletePost();
+    }
+  };
+
+  return (
+    <button onClick={handlePostDelete} disabled={isDeleting}>
+      {isDeleting ? "Deleting..." : "Delete"}
+    </button>
+  );
 }
 
 export default DeletePost;
