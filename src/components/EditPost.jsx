bd9fcbba4f2c3bf546765a51a38d28146dd95aac
Zohra <zohra.boukhatem1@gmail.com>
2023-11-20 16:54:43 +0100

    did editPost functionality, added toggle option for the edit form in allposts page

--- src/components/EditPost.jsx
+++ src/components/EditPost.jsx
@@ -1,7 +1,165 @@
-function EditPost() {
-    return (
-        <button>Edit</button>
-    )
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+const API_URL = "https://random-news-react-app.adaptable.app/posts";
+
+function EditPost({postId, post}) {
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
+  const [date, setDate] = useState("");
+  const [category, setCategory] = useState("other");
+  const [imageURL, setImageURL] = useState("");
+  const [URL, setURL] = useState("");
+  const [editedPost, setEditedPost] = useState(post)
+  
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    // const requestBody = {
+    //   title,
+    //   description,
+    //   location,
+    //   date,
+    //   category,
+    //   imageURL,
+    //   URL,
+    // };
+
+    axios
+      .put(`${API_URL}/${postId}`, editedPost)
+      .then(() => {
+        console.log("Post edited");
+      })
+      .catch((error) => {
+        console.log("Error updating post...");
+        console.log(error);
+      });
+  };
+ 
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <label className="form">
+        Title:
+        <input
+          type="text"
+          name="title"
+          placeholder="Enter title"
+          required={true}
+          value={editedPost.title}
+          onChange={(e) => {
+            setEditedPost(prev => ({...prev, [e.target.name] : e.target.value}))
+            // setTitle(e.target.value);
+          }}
+        />
+      </label>
+      <br />
+      <label className="form">
+        Description:
+        <textarea
+          type="text"
+          name="description"
+          placeholder="Write a description"
+          required={true}
+          value={editedPost.description}
+          onChange={(e) => {
+            setEditedPost(prev => ({...prev, [e.target.name] : e.target.value}))
+
+            // setDescription(e.target.value);
+          }}
+        />
+      </label>
+      <br />
+      <label className="form">
+        Location:
+        <input
+          type="text"
+          name="location"
+          placeholder="Specify the location"
+          value={editedPost.location}
+          onChange={(e) => {
+            setEditedPost(prev => ({...prev, [e.target.name] : e.target.value}))
+
+            // setLocation(e.target.value);
+          }}
+        />
+      </label>
+      <br />
+      <label className="form">
+        Date:
+        <input
+          type="date"
+          name="date"
+          placeholder="Set the date"
+          value={editedPost.date}
+          onChange={(e) => {
+            setEditedPost(prev => ({...prev, [e.target.name] : e.target.value}))
+
+            // setDate(e.target.value);
+          }}
+        />
+      </label>
+      <br />
+      <label className="form">
+        Image:
+        <input
+          type="text"
+          name="imageURL"
+          placeholder="Paste image URL"
+          value={editedPost.imageURL}
+          onChange={(e) => {
+            setEditedPost(prev => ({...prev, [e.target.name] : e.target.value}))
+
+            // setImageURL(e.target.value);
+          }}
+        />
+      </label>
+      <br />
+      <label className="form">
+        URL:
+        <input
+          type="text"
+          name="URL"
+          placeholder="Add a URL"
+          value={editedPost.URL}
+          onChange={(e) => {
+            setEditedPost(prev => ({...prev, [e.target.name] : e.target.value}))
+
+            // setURL(e.target.value);
+          }}
+        />
+      </label>
+      <br />
+      <label className="form">
+        Choose Category:
+        <select
+          name="category"
+          value={editedPost.category}
+          onChange={(e) => {
+            setEditedPost(prev => ({...prev, [e.target.name] : e.target.value}))
+
+            // setCategory(e.target.value);
+          }}
+        >
+          <option value="weather">Weather</option>
+          <option value="selling">Selling</option>
+          <option value="entertainment">Entertainment</option>
+          <option value="events">Events</option>
+          <option value="traffic">Traffic</option>
+          <option value="social">Social</option>
+          <option value="jobs">Jobs</option>
+          <option value="education">Education</option>
+          <option value="other">Other</option>
+        </select>
+      </label>
+      <br />
+      <button type="submit">Edit</button>
+    </form>
+  );
 }
 
-export default EditPost
+export default EditPost;
--- src/pages/AllPostsPage.jsx
+++ src/pages/AllPostsPage.jsx
@@ -13,7 +13,7 @@ function AllPostsPage() {
   const [posts, setPosts] = useState([]);
   const [fetching, setFeching] = useState(true);
   const [displayedPosts, setDisplayedPosts] = useState(6);
-
+  const [toggleEdit, setToggleEdit] = useState({ toggle: false, index: null }); //initial state for toggling
   useEffect(() => {
     axios
       .get(API_URL)
@@ -37,12 +37,16 @@ function AllPostsPage() {
       .catch((error) => error);
   };
 
+  useEffect(() => {
+    console.log(toggleEdit);
+  }, [toggleEdit]);
   return (
     <section className="all-posts">
+      
       {fetching ? (
         <Loader />
       ) : (
-        posts.slice(0, displayedPosts).map((post) => {
+        posts.slice(0, displayedPosts).map((post, i) => {
           return (
             <div key={post.id} className="post-container">
               <NavLink to={`/posts/${post.id}`}>
@@ -64,7 +68,27 @@ function AllPostsPage() {
                 <h2>{post.date}</h2>
                 <h3>Category: {post.category}</h3>
               </NavLink>
-              <EditPost />
+              <button
+                onClick={
+                  () =>
+                    toggleEdit.toggle && i !== toggleEdit.index //checks if toggle is true and index is different from current post
+                      ? setToggleEdit((prev) => ({
+                          ...prev, // if so, we are just clicking edit for another post, so change index
+                          index: i
+                        }))
+                      : setToggleEdit((prev) => ({
+                          toggle: !prev.toggle,
+                          index: !prev.toggle ? i : null,
+                        })) // if i is not different from current index, we hide the edit
+                }
+              >
+                Edit post
+              </button>
+              
+              {toggleEdit.toggle && toggleEdit.index === i && ( // if toggle is true we show the edit form (also checking index to make sure we open the right form)
+                <EditPost post={post} postId={post.id} />
+              )}
+
               <DeletePost postId={post.id} onDelete={handlePostDelete} />
             </div>
           );
