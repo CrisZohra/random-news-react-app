ac1136d62ea27880af8c938a868dbac2ef46df0b
Cristina RP <cristinarp95@gmail.com>
2023-11-22 14:14:20 +0100

    Add header to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,18 @@
 import AddPost from "../components/AddPost";
 import AllPosts from "../components/AllPosts";
 import Footer from "../components/Footer";
-import titlePic from "/title4.png"
+import titlePic from "/title4.png";
 function HomePage() {
   return (
     <>
-      <img src={titlePic} alt="" />
+      <section className="homepage-header">
+        <img src={titlePic} alt="what now? title" />
+        <div className="homepage-header-text">
+          <h4>Unleash Local Stories, From Weather Whims to Job Wins!</h4>
+          <h4>Post, Share, Connect - Every Detail, Every Moment.</h4>
+          <h4>Dive in, Post Now!</h4>
+        </div>
+      </section>
       <AllPosts />
       <AddPost />
       <Footer />
